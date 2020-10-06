539847cd602288f1aa577ad3cdc8d2de23a74055
weidaokuo <63395449+weidaokuo@users.noreply.github.com>
2020-10-06 16:17:47 +0800

    Update and rename pridict.py to predict.py

--- predict.py
+++ predict.py
@@ -5,15 +5,15 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 预处理
+# 预处理，提高图片的输入分辨率后检测速度变慢。
 x_transforms = transforms.Compose([
-    transforms.Resize((1568, 1568)),
+    transforms.Resize((1568, 1568)),         
     # transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
 ])
 
-# load image
+# 加载图片
 img = Image.open("./data/text/01.jpg")
 # plt.imshow(img)
 # plt.show()
@@ -25,16 +25,13 @@ img = x_transforms(img)
 img = torch.unsqueeze(img, dim=0)
 
 
-# create model
+# 创建模型
 model = Unet(3, 1)
-# load model weights
+# 加载模型权重
 model_weight_path = "./original_weights_20.pth"
 model.load_state_dict(torch.load(model_weight_path))
 model.eval()
 
-# def uint82bin(n, count=8):
-#     """returns the binary of integer n, count refers to amount of bits"""
-#     return ''.join([str((n >> y) & 1) for y in range(count-1, -1, -1)])
 
 
 with torch.no_grad():
